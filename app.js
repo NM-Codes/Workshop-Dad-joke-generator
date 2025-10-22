b95a0dd349dad11f218d952d0a1a62ce03c597b3
NM Codes <nm.codes@hotmail.com>
2025-10-22 10:47:12 +0200

    skrivit klart pseudo koden i js filen.

--- app.js
+++ app.js
@@ -27,3 +27,16 @@
 // jokeBtn
 
 // 2. Ropa funktionen generateJoke()
+
+// 3. Klickevent på jokeBtn:  när den klickas kör funktionen generateJoke()
+
+// 4. Funktion generateJoke():
+// 4.2 gör en inställning - säg till servern: vill ha svar i json-format
+// 4.3 Hämta ett skämt från adressen http:icanhazdadjoke.com
+// 4.4 Om det har gått bra 
+// gör om svaret till vanlig text
+// skriv skämtet i jokeElement
+// 4.5 Om det har blivit fel
+// skriv "Oops! Inget skämt..." i jokeElement
+// skriv felet i konsolen så att man kan felsöka det om så behövs.
+
